c0c5366ea3e9902db815246e07737bb6ca98f5eb
Scott Charlton <charlton@usgs.gov>
2017-05-08 16:19:40 -0600

    updated qwt build

--- build_2013_4.cmd
+++ build_2013_4.cmd
@@ -6,17 +6,21 @@ call "C:\Qt\5.5\msvc2013_64\bin\qtenv2.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 popd
 
-if exist lib\src\qwt-6.1.3   rd /s /q lib\src\qwt-6.1.3
-if exist lib\build\qwt-6.1.3 rd /s /q lib\build\qwt-6.1.3
+call versions.cmd
+set VER=%QWT-VER%
 
-7z x qwt-6.1.3.zip -o"lib\src"
+if exist lib\src\qwt-%VER%     rd /s /q lib\src\qwt-%VER%
+if exist lib\build\qwt-%VER%   rd /s /q lib\build\qwt-%VER%
+if exist lib\install\qwt-%VER% rd /s /q lib\install\qwt-%VER%
+
+7z x qwt-%VER%.zip -o"lib\src"
 
 # build and install debug
-pushd lib\src\qwt-6.1.3
+pushd lib\src\qwt-%VER%
 qmake
 nmake all
 nmake install
-move c:\Qwt-6.1.3 ..\..\install\
+move c:\Qwt-%VER% ..\..\install\
 popd
 
 endlocal
