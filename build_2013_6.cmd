b1395af42dcdee924946679da285820d7384861f
Scott Charlton <charlton@usgs.gov>
2017-05-08 16:49:51 -0600

    updated proj build

--- build_2013_6.cmd
+++ build_2013_6.cmd
@@ -6,17 +6,20 @@ call "C:\Qt\5.5\msvc2013_64\bin\qtenv2.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 popd
 
-if exist lib\src\proj-4.8.0     rd /s /q lib\src\proj-4.8.0
-if exist lib\build\proj-4.8.0   rd /s /q lib\build\proj-4.8.0
-if exist lib\install\proj-4.8.0 rd /s /q lib\install\proj-4.8.0
+call versions.cmd
+set VER=%PROJ-VER%
 
-7z x proj-4.8.0.zip -o"lib\src"
+if exist lib\src\proj-%VER%     rd /s /q lib\src\proj-%VER%
+if exist lib\build\proj-%VER%   rd /s /q lib\build\proj-%VER%
+if exist lib\install\proj-%VER% rd /s /q lib\install\proj-%VER%
+
+7z x proj-%VER%.zip -o"lib\src"
 
 set topdir=%~dp0
 
-pushd lib\src\proj-4.8.0
-nmake -f makefile.vc INSTDIR=%topdir%\lib\install\proj-4.8.0\release
-nmake -f makefile.vc INSTDIR=%topdir%\lib\install\proj-4.8.0\release install-all
+pushd lib\src\proj-%VER%
+nmake -f makefile.vc INSTDIR=%topdir%\lib\install\proj-%VER%\release
+nmake -f makefile.vc INSTDIR=%topdir%\lib\install\proj-%VER%\release install-all
 popd
 
 endlocal
