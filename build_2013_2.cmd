9175ccf3f206205deaab63d9a53955f2a7851c0c
Scott Charlton <charlton@usgs.gov>
2017-05-08 15:53:00 -0600

    updated cgns script

--- build_2013_2.cmd
+++ build_2013_2.cmd
@@ -1,16 +1,22 @@
 @echo off
 setlocal enableextensions
 
-set GENERATOR="Visual Studio 12 2013 Win64"
-set SGEN="vs2013-x64"
+if [%GENERATOR%]==[] (
+  echo No Generator has been set
+  exit /b 1
+)
 
-if exist lib\src\cgnslib-3.2.1   rd /s /q lib\src\cgnslib-3.2.1
-if exist lib\build\cgnslib-3.2.1 rd /s /q lib\build\cgnslib-3.2.1
+call versions.cmd
+set VER=%CGNSLIB-VER%
 
-7z x cgnslib_3.2.1.tar.gz -so | 7z x -si -ttar -o"lib\src"
-ren lib\src\cgnslib_3.2.1 cgnslib-3.2.1
+if exist lib\src\cgnslib-%VER%     rd /s /q lib\src\cgnslib-%VER%
+if exist lib\build\cgnslib-%VER%   rd /s /q lib\build\cgnslib-%VER%
+if exist lib\install\cgnslib-%VER% rd /s /q lib\install\cgnslib-%VER%
 
-ctest -S build-cgnslib.cmake -DCONF_DIR:STRING=debug   -DCTEST_CMAKE_GENERATOR:STRING=%SGEN% -DCTEST_CMAKE_GENERATOR:STRING=%GENERATOR% -C Debug   -VV -O build-cgnslib-debug.log
-ctest -S build-cgnslib.cmake -DCONF_DIR:STRING=release -DCTEST_CMAKE_GENERATOR:STRING=%SGEN% -DCTEST_CMAKE_GENERATOR:STRING=%GENERATOR% -C Release -VV -O build-cgnslib-release.log
+7z x cgnslib_%VER%.tar.gz -so | 7z x -si -ttar -o"lib\src"
+ren lib\src\cgnslib_%VER% cgnslib-%VER%
+
+ctest -S build-cgnslib.cmake -DCONF_DIR:STRING=debug   -DCTEST_CMAKE_GENERATOR:STRING=%GENERATOR% -C Debug   -VV -O %SGEN%-cgnslib-debug.log
+ctest -S build-cgnslib.cmake -DCONF_DIR:STRING=release -DCTEST_CMAKE_GENERATOR:STRING=%GENERATOR% -C Release -VV -O %SGEN%-cgnslib-release.log
 
 endlocal
