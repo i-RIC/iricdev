787714005dff87588d4f6163f210de77b553f41c
Scott Charlton <charlton@usgs.gov>
2017-06-07 23:35:50 -0600

    moving cmake directory breaks find_package(HDF5 NAMES hdf5))

--- build-hdf5.sh
+++ build-hdf5.sh
@@ -21,9 +21,3 @@ cd ../..
 ctest -S build-hdf5.cmake -DCONF_DIR:STRING=debug   -DCTEST_CMAKE_GENERATOR:STRING="Unix Makefiles" -C Debug   -V -O $SGEN-hdf5-debug.log
 
 ctest -S build-hdf5.cmake -DCONF_DIR:STRING=release -DCTEST_CMAKE_GENERATOR:STRING="Unix Makefiles" -C Release -V -O $SGEN-hdf5-release.log
-
-mv lib/install/hdf5-$VER/debug/share/*   lib/install/hdf5-$VER/debug/.
-mv lib/install/hdf5-$VER/release/share/* lib/install/hdf5-$VER/release/.
-
-rmdir lib/install/hdf5-$VER/debug/share
-rmdir lib/install/hdf5-$VER/release/share
