dbea25987663af35522f5755cb4959ef9e981620
Scott Charlton <charlton@usgs.gov>
2017-06-12 23:10:27 -0600

    minimized for solution builds and added iriclib

--- build_2013.cmd
+++ build_2013.cmd
@@ -9,17 +9,18 @@ popd
 set GENERATOR="Visual Studio 12 2013 Win64"
 set SGEN="vs2013-x64"
 
-call build-vtk.cmd
+REM call build-vtk.cmd
 call build-hdf5.cmd
 call build-cgnslib.cmd
-call build-shapelib.cmd
-call build-qwt.cmd
-call build-gdal.cmd
-call build-proj.cmd
-call build-netcdf.cmd
-call build-geos.cmd
-call build-boost.cmd
-call build-yaml-cpp.cmd
+call build-iriclib.cmd
+REM call build-shapelib.cmd
+REM call build-qwt.cmd
+REM call build-gdal.cmd
+REM call build-proj.cmd
+REM call build-netcdf.cmd
+REM call build-geos.cmd
+REM call build-boost.cmd
+REM call build-yaml-cpp.cmd
 
 call create-paths-pri.cmd > paths.pri
 call create-dirExt-prop.cmd > dirExt.prop
