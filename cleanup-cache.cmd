914d94f223d71d979e8c5088659adea4a1f9c928
Scott Charlton <charlton@usgs.gov>
2019-09-26 19:31:17 -0600

    added additional cleanup

--- cleanup-cache.cmd
+++ cleanup-cache.cmd
@@ -10,3 +10,4 @@ if EXIST lib\install\iriclib-aabe9ea rd /s /q lib\install\iriclib-aabe9ea
 if EXIST lib\install\iriclib-a6a110f rd /s /q lib\install\iriclib-a6a110f
 if EXIST lib\install\iriclib-fbefd63 rd /s /q lib\install\iriclib-fbefd63
 if EXIST lib\install\iriclib-7cc7034 rd /s /q lib\install\iriclib-7cc7034
+if EXIST lib\install\iriclib-2cc80da rd /s /q lib\install\iriclib-2cc80da
